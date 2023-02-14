133496e783850e680352350c1a56adc3daa2d071
clementreiffers <reiffersclement@gmail.com>
2023-02-14 14:17:41 +0100

    refacto

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,12 @@
 #define YOLO_CFG_FILE MODEL_PATH "yolo/yolov4-tiny.cfg"
 #define YOLO_CLASS_NAMES MODEL_PATH "yolo/classes_names_yolo.txt"
 
+#define IMAGE_PADDING 20
+
+#define RED CV_RGB(255, 0, 0)
+#define GREEN CV_RGB(0, 255, 0)
+#define BLUE CV_RGB(0, 0, 255)
+
 using namespace cv;
 using namespace std;
 using namespace dnn;
@@ -101,7 +107,8 @@ long long computeDuration(time_point<steady_clock> &start) {
   return duration_cast<microseconds>(getNow() - start).count() / 1000;
 }
 
-void postProcessing(const vector<Mat> &outs, const Net &net, Mat img) {
+void postProcessing(const vector<Mat> &outs, const Net &net, Mat img,
+                    Scalar color) {
   float confidenceThreshold = 0.33;
 
   vector<int> classIds;
@@ -126,75 +133,102 @@ void postProcessing(const vector<Mat> &outs, const Net &net, Mat img) {
 
         classIds.push_back(classIdPoint.x);
         confidences.push_back(confidence);
-        rectangle(img, Rect(left, top, width, height), Scalar(0, 255, 0), 2);
+        rectangle(img, Rect(left, top, width, height), color, 2);
       }
     }
   }
 }
 
-int main() {
+vector<string> readClassNames(const string &fileName) {
+  vector<string> classNames;
+  std::ifstream file(fileName);
+  if (file.is_open()) {
+    std::string className;
+    while (getline(file, className)) {
+      classNames.push_back(className);
+    }
+  }
+  return classNames;
+}
 
+Mat setPadding(Mat img) {
+  int padding = 50;
+  Mat padded_image(img.size().height + 2 * padding,
+                   img.size().width + 2 * padding, CV_8UC3,
+                   cv::Scalar(0, 0, 0));
+
+  img.copyTo(padded_image(
+      cv::Rect(padding, padding, img.size().width, img.size().height)));
+  return padded_image;
+}
+
+string setStringFormat(const string &className, double confidence) {
+  return format("%s %.2f", className.c_str(), confidence);
+}
+
+int main() {
   vector<Mat> imageMat =
       randomizeVectorMat(readImageVector(getAllImageFiles(IMAGE_PATH_DIR)));
-  Mat blob;
 
-  for (const auto &image : imageMat) {
-    imshow("image", image);
-    blobFromImage(image, blob, 1., Size(416, 416), Scalar(), true);
-    ifstream ifs(GOOGLE_CLASS_NAMES);
-    if (!ifs.is_open()) {
-      cerr << GOOGLE_CLASS_NAMES << " not found!" << endl;
-    }
+  vector<string> googleClassName = readClassNames(GOOGLE_CLASS_NAMES);
+  vector<string> yoloClassName   = readClassNames(YOLO_CLASS_NAMES);
 
-    vector<string> classes;
-    string line;
-    while (getline(ifs, line)) {
-      classes.push_back(line);
-    }
+  Net modelGoogle                = readNet(GOOGLE_MODEL_FILE, GOOGLE_CFG_FILE);
+  Net modelYolo                  = readNet(YOLO_MODEL_FILE, YOLO_CFG_FILE);
 
-    Net modelGoogle = readNet(GOOGLE_MODEL_FILE, GOOGLE_CFG_FILE);
-    Net modelYolo   = readNet(YOLO_MODEL_FILE, YOLO_CFG_FILE);
+  for (auto &img : imageMat) {
+    auto start = high_resolution_clock::now();
 
-    for (const auto &img : imageMat) {
-      auto start  = high_resolution_clock::now();
-      int padding = 50;
-      Mat padded_image(img.size().height + 2 * padding,
-                       img.size().width + 2 * padding, CV_8UC3,
-                       cv::Scalar(0, 0, 0));
+    Mat blob;
+    blobFromImage(img, blob, 1., Size(416, 416), Scalar(), true);
 
-      img.copyTo(padded_image(
-          cv::Rect(padding, padding, img.size().width, img.size().height)));
+    //    img        = setPadding(img);
 
-      blobFromImage(padded_image, blob, 1., Size(224, 224),
-                    Scalar(104, 117, 123), true);
-      modelGoogle.setInput(blob);
-      modelYolo.setInput(blob, "", 0.00392, Scalar(0, 0, 0));
-      Mat probGoogle = modelGoogle.forward();
+    blobFromImage(img, blob, 1., Size(224, 224), Scalar(104, 117, 123), true);
 
-      Point classIdPoint;
-      double confidence;
-      minMaxLoc(probGoogle, nullptr, &confidence, nullptr, &classIdPoint);
-      int classId                 = classIdPoint.x;
+    modelGoogle.setInput(blob, "", 0.00392, Scalar(0, 0, 0));
+    modelYolo.setInput(blob, "", 0.00392, Scalar(0, 0, 0));
 
-      vector<string> outNames     = modelGoogle.getUnconnectedOutLayersNames();
-      vector<string> outNamesYolo = modelYolo.getUnconnectedOutLayersNames();
-      vector<Mat> outsGoogle, outsYolo;
-      modelGoogle.forward(outsGoogle, outNames);
-      modelYolo.forward(outsYolo, outNamesYolo);
+    Mat probGoogle = modelGoogle.forward();
+    Mat probYolo   = modelYolo.forward();
 
-      string label = format("%s: %2.f", classes[classId].c_str(), confidence);
+    Point classIdPointGoogle, classIdPointYolo;
 
-      long long countMiliseconds = computeDuration(start);
+    double googleConfidence, yoloConfidence;
 
-      cout << "execution time in miliseconds : " << countMiliseconds << endl;
-      waitKey(1000);
-      postProcessing(outsYolo, modelYolo, padded_image);
+    minMaxLoc(probGoogle, nullptr, &googleConfidence, nullptr,
+              &classIdPointGoogle);
+    int classIdGoogle = classIdPointGoogle.x;
 
-      putText(padded_image, label, Point(0, padded_image.rows - 7),
-              FONT_HERSHEY_SIMPLEX, 0.8, CV_RGB(0, 255, 0), 2, LINE_AA);
+    minMaxLoc(probYolo, nullptr, &yoloConfidence, nullptr, &classIdPointYolo);
+    int classIdYolo               = classIdPointYolo.x;
 
-      imshow("image", padded_image);
-      waitKey(1000);
-    }
+    vector<string> outNamesGoogle = modelGoogle.getUnconnectedOutLayersNames();
+    vector<string> outNamesYolo   = modelYolo.getUnconnectedOutLayersNames();
+    vector<Mat> outsGoogle, outsYolo;
+
+    modelGoogle.forward(outsGoogle, outNamesGoogle);
+    modelYolo.forward(outsYolo, outNamesYolo);
+
+    string googleLabel =
+        setStringFormat(googleClassName[classIdGoogle], googleConfidence);
+    string yoloLabel =
+        setStringFormat(yoloClassName[classIdYolo], yoloConfidence);
+
+    long long countMiliseconds = computeDuration(start);
+
+    postProcessing(outsYolo, modelYolo, img, RED);
+    postProcessing(outsGoogle, modelGoogle, img, GREEN);
+
+    putText(img, googleLabel, Point(0, img.rows - 7), FONT_HERSHEY_SIMPLEX, 0.8,
+            GREEN, 1, LINE_AA);
+
+    putText(img, yoloLabel, Point(0, img.rows - 60), FONT_HERSHEY_SIMPLEX, 0.8,
+            RED, 1, LINE_AA);
+
+    cout << "execution time in miliseconds : " << countMiliseconds << endl;
+
+    imshow("image", img);
+    waitKey(1000);
   }
 }
